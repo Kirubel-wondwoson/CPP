5c03c4f298354a30203044e15af3257edd2faea6
Kirubel Wondwoson <124454603+Kirubel-wondwoson@users.noreply.github.com>
2024-02-07 00:20:38 +0300

    Add files via upload

---
+++ User defined datatypes/Exercise on structre/Exercise1V3_usingClass.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+
+class student{
+    public:
+    string id, name;
+    float testMark;
+    float finalMark;
+    float totalMark;
+    
+    float add(float tmark, float fmark){
+        totalMark = 
+    }
+};
+
+int main()
+{
+    student student1;
+    cout<<"Enter your name : ";
+    cin>>student1.name;
+    cout<<"Enter your id : ";
+    cin>>student1.id;
+    cout<<"Enter your test result : ";
+    cin>>student1.testMark;
+    cout<<"Enter your final result : ";
+    cin>>student1.finalMark;
+    result(student1.testMark,student1.finalMark);
+    return 0;
+}
