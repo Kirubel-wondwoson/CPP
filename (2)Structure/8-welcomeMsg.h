5fa97b803e02fce46d93f1df3708d5a74172e790
Kirubel Wondwoson <124454603+Kirubel-wondwoson@users.noreply.github.com>
2024-02-07 00:17:23 +0300

    Delete (2)Structure directory

--- (2)Structure/8-welcomeMsg.h
+++
@@ -1,8 +0,0 @@
-#ifndef WELCOMEMSG_H 
-#define WELCOMEMSG_H 
-#include"8-LoginPage.h"       
-#include<iostream>
-void welcomeMsg(char name[10]){
-    std::cout<<"welcome "<<name[10]<<std::endl;
-}
-#endif
--- (2)Structure/9-powLibrary.h
+++
@@ -1,7 +0,0 @@
-#ifndef POWLIBRARY_H
-#define POWLIBRAY_H
-mypow(float __x , float __y)
-{
-    return __builtin_powf(__x , __y);
-}
-#endif
